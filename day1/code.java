fad06ecf6ffddaa67758a58b1b0da5fa2ce11493
Divyauma33 <divyauma33@gmail.com>
2026-02-17 22:22:37 +0530

    Update code.java

--- day1/code.java
+++ day1/code.java
@@ -1,1 +1,12 @@
-
+program 1:
+public class Main{
+  public static void main(String[] args){
+    System.out.println("my name is Divya");
+    System.out.println("I am Learning java");
+  }
+  program 2:
+  public class Main{
+    public static void main(String[] args){
+      System.out.println("my age is 18");
+      System.out .println("**");
+    }
