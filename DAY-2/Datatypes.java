c048c4822bc7a09be96ca88c08d8fd633a683cef
Divyauma33 <divyauma33@gmail.com>
2026-02-18 22:42:31 +0530

    Create Datatypes.java

---
+++ DAY-2/Datatypes.java
@@ -0,0 +1,12 @@
+ublic class Datatypes{
+    public static void main(String[] args){
+        int age=19;
+        double CGPA=8.9;
+        char initial='D';
+        boolean likescoding=true;
+        System.out.println("Age:" + age);
+        System.out.println("CGPA:" + CGPA);
+        System.out.println("Initial:" + initial);
+        System.out.println("likes coding:" +likescoding );
+    }
+}
