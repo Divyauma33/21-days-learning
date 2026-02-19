ef744c41e18c16a8c0a9ef88766d66437ea666be
Divyauma33 <divyauma33@gmail.com>
2026-02-19 22:41:16 +0530

    Create Operators.java

---
+++ DAY-3/Operators.java
@@ -0,0 +1,44 @@
+public class Operators{
+    public static void main(String[] args){
+        int a=10;
+        int b=3;
+        //Arithmetic operation
+        //System.out.println(a+b);
+        //System.out.println(a-b);
+       // System.out.println(a*b);
+       // System.out.println(a/b);
+        //System.out.println(a%b);
+
+        //Relational operation return boolean
+        //System.out.println(a>b);
+        //System.out.println(a<b);
+        //System.out.println(a==b);
+        //System.out.println(a!=b);
+
+        //Logical operators
+        //int age=20;
+        //System.out.println(age>18 && age<30);
+        //System.out.println(age>40 || age<21);
+        //System.out.println(age>=18 && age<=30);
+        //System.out.println(!(age>18));
+
+        //Assignment Operators
+       // int num=10;
+        //num+=5;
+        //num-=5;
+        //num*=5;
+        //num/=5;
+        //System.out.println(num);
+
+        //Increment and decrement
+
+        int x=5;
+        System.out.println(++x);
+        System.out.println(x++);
+        System.out.println(x);
+        
+
+
+
+    }
+}
