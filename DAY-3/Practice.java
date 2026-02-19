ed5c1831247bd59ff4204fbcd5404c81535eaa1a
Divyauma33 <divyauma33@gmail.com>
2026-02-19 22:42:06 +0530

    Create Practice.java

---
+++ DAY-3/Practice.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Practice{
+    public static void main(String[] args){
+        //double a=5;
+        //double b=10;
+        //System.out.println(a+b);
+        //System.out.println(a-b);
+        //System.out.println(a*b);
+        //System.out.println(a/b);
+        //System.out.println(a%b);
+
+        //Scanner scan=new Scanner(System.in);
+        //int num =scan.nextInt();
+        //System.out.println(num%=0);
+
+
+
+
+        
+    }
+}
