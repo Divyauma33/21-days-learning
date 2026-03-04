0298a345823e63594f129a4ebef6a451966e7b24
Divyauma33 <divyauma33@gmail.com>
2026-03-04 21:25:31 +0530

    Create class and object.java

---
+++ Day-16/class and object.java
@@ -0,0 +1,31 @@
+/*class Student{
+    String name;
+    int age;
+    void display(){
+        System.out.println(name + " " + age);
+    }
+}
+public class Topic{
+    public static void main(String[]  args){
+        Student s1=new Student();
+        s1.name="Divya";
+        s1.age=20;
+        s1.display();
+    }
+}*/
+
+/*class Mobile{
+    String brand;
+    int price;
+    void displayMobile(){
+        System.out.println(brand + " " + price);
+    }
+}
+public class Topic{
+    public static void main(String[] args){
+        Mobile m1=new Mobile();
+        m1.brand="OPPO";
+        m1.price=20000;
+        m1.displayMobile();
+    }
+}*/
